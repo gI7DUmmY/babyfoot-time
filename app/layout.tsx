d7854d4055d75c772641f089ac262c9e5fb133b0
Cedric Rollo <cedric.rollo@gmail.com>
2024-04-29 20:23:41 +0200

    🔍️ metadatas

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,12 @@ export const metadata: Metadata = {
     'twitter:image':
       'https://firebasestorage.googleapis.com/v0/b/tft---site.appspot.com/o/tft_officiel.png?alt=media&token=eaabc2b1-206a-4aff-bed4-025ed922cbfe',
     'twitter:card': 'summary_large_image',
-    'twitter:domain': 'tft-eight.vercel.app',
-    'twitter:url': 'https://tft-eight.vercel.app/',
+    'twitter:domain': 'babyfoot-time.vercel.app',
+    'twitter:url': 'https://babyfoot-time.vercel.app',
     'twitter:title': 'Babyfoot Time',
     'twitter:description':
       "Babyfoot Time : spécialiste de l'animation autour du baby-foot pour vos évènements privés et publics: personnels et professionnels. Ambiance chaleureuse et conviviale garantie! Location de baby-foot Bonzini : modèle officiel pour la compétition. Transport, montage, réglage et démontage des tables. Pour s'amuser, favoriser les échanges et se défouler. Activité mixte, intergénérationnelle, ludique et originale.",
-    'og:url': 'https://tft-eight.vercel.app/',
+    'og:url': 'https://babyfoot-time.vercel.app',
     'og:image':
       'https://firebasestorage.googleapis.com/v0/b/tft---site.appspot.com/o/tft_officiel.png?alt=media&token=eaabc2b1-206a-4aff-bed4-025ed922cbfe',
     'og:type': 'website',
