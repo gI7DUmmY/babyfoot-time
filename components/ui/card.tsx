ec27c470fd1d576bb6e4bcb03886f795402555dd
Cedric Rollo <cedric.rollo@gmail.com>
2024-05-01 17:51:54 +0200

    📱 font sizes

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -33,7 +33,7 @@ const CardTitle = React.forwardRef<
   <h3
     ref={ref}
     className={cn(
-      'text-2xl font-playfair font-semibold leading-none tracking-tight text-orange2 uppercase text-center',
+      'text-2xl font-playfair font-semibold leading-none tracking-tight text-orange2 uppercase text-center lg:text-4xl',
       className
     )}
     {...props}
@@ -59,7 +59,10 @@ const CardContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('p-6 pt-0 uppercase text-center font-lora', className)}
+    className={cn(
+      'p-6 pt-0 uppercase text-center font-lora lg:text-2xl',
+      className
+    )}
     {...props}
   />
 ))
