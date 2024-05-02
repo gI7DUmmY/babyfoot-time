07776d55a26a4cdde3df3c18af55aa1de3917e37
Cedric Rollo <cedric.rollo@gmail.com>
2024-05-02 21:03:04 +0200

    💄🍱 favicon

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import banniere from '@/public/assets/images/logo2.svg'
+import banniere from '@/public/assets/images/banniere.svg'
 
 import Titre from '@/components/Titre'
 import Services from '@/components/Services'
