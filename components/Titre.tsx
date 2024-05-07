58c7fd76611ba8d9560b155e7321b79b0197c414
Cedric Rollo <cedric.rolllo@iut-tlse3.fr>
2024-05-07 10:51:31 +0200

    📱titre

--- components/Titre.tsx
+++ components/Titre.tsx
@@ -4,7 +4,7 @@ import event from '@/public/assets/images/event_orange1.png'
 const Titre = () => {
   return (
     <section className='parallax mt-36 h-[85vh] md:mt-52 lg:mt-60' id='titre'>
-      <h1 className='text-2xl text-center uppercase pt-6 px-2 font-display font-extrabold drop-shadow-white lg:text-4xl'>
+      <h1 className='text-2xl text-center uppercase pt-6 px-2 font-display font-extrabold drop-shadow-white text-pretty lg:text-3xl lg:px-6 xl:text-4xl'>
         spécialiste de l'animation autour du{' '}
         <span className='text-orange1'>baby-foot</span>
       </h1>
