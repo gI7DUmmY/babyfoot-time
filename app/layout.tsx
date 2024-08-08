71bcae4acc68ad4dcc51f30358c2ede1f3f70fac
Cedric Rollo <cedric.rollo@gmail.com>
2024-08-08 13:47:17 +0200

    🚨 metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { playfair_display, playfair, lora } from './fonts'
 import './globals.css'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://babyfoot-time.fr'),
   title: 'Babyfoot Time',
   description:
     "Babyfoot Time : spécialiste de l'animation autour du baby-foot pour vos évènements privés et publics: personnels et professionnels. Ambiance chaleureuse et conviviale garantie! Location de baby-foot Bonzini : modèle officiel pour la compétition. Transport, montage, réglage et démontage des tables. Pour s'amuser, favoriser les échanges et se défouler. Activité mixte, intergénérationnelle, ludique et originale.",
@@ -10,7 +11,7 @@ export const metadata: Metadata = {
     title: 'Babyfoot Time',
     description:
       "Babyfoot Time : spécialiste de l'animation autour du baby-foot pour vos évènements privés et publics: personnels et professionnels. Ambiance chaleureuse et conviviale garantie! Location de baby-foot Bonzini : modèle officiel pour la compétition. Transport, montage, réglage et démontage des tables. Pour s'amuser, favoriser les échanges et se défouler. Activité mixte, intergénérationnelle, ludique et originale.",
-    url: 'https://babyfoot-time.vercel.app',
+    url: 'https://babyfoot-time.fr',
     siteName: 'Babyfoot Time',
     images: [
       {
@@ -27,8 +28,8 @@ export const metadata: Metadata = {
     'twitter:image':
       'https://firebasestorage.googleapis.com/v0/b/tft---site.appspot.com/o/logo_babyfoot-time%20v2.png?alt=media&token=0f5fa164-f97e-48d9-a8a0-18192a9d60c9',
     'twitter:card': 'summary_large_image',
-    'twitter:domain': 'babyfoot-time.vercel.app',
-    'twitter:url': 'https://babyfoot-time.vercel.app',
+    'twitter:domain': 'babyfoot-time.fr',
+    'twitter:url': 'https://babyfoot-time.fr',
     'twitter:title': 'Babyfoot Time',
     'twitter:description':
       "Babyfoot Time : spécialiste de l'animation autour du baby-foot pour vos évènements privés et publics: personnels et professionnels. Ambiance chaleureuse et conviviale garantie! Location de baby-foot Bonzini : modèle officiel pour la compétition. Transport, montage, réglage et démontage des tables. Pour s'amuser, favoriser les échanges et se défouler. Activité mixte, intergénérationnelle, ludique et originale.",
