5d9cb8e27fd70409223647f97202fb30f9fa3268
Cedric Rollo <cedric.rolllo@iut-tlse3.fr>
2024-12-13 10:34:07 +0100

    💄📱 feeback

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -29,7 +29,7 @@ const Feedback = () => {
                       de prochaines manifestations ludiques&nbsp;! &raquo;
                     </p>
                   </blockquote>
-                  <div className='text-slate-500 capitalize italic text-base'>
+                  <div className='text-slate-500 capitalize italic text-base mt-2 md:text-lg'>
                     Matthieu, organisateur du Festival du jeu
                   </div>
                 </CardContent>
@@ -47,7 +47,7 @@ const Feedback = () => {
                       de la compétition &raquo;
                     </p>
                   </blockquote>
-                  <div className='text-slate-500 capitalize italic text-base'>
+                  <div className='text-slate-500 capitalize italic text-base mt-2 md:text-lg'>
                     Aurore, animatrice collège
                   </div>
                 </CardContent>
@@ -65,50 +65,13 @@ const Feedback = () => {
                       baby-foot au père Noël !? &raquo;
                     </p>
                   </blockquote>
-                  <div className='text-slate-500 capitalize italic text-base'>
+                  <div className='text-slate-500 capitalize italic text-base mt-2 md:text-lg'>
                     Un papa qui jouait avec son fils
                   </div>
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
-
-          {/* <CarouselItem className='md:basis-1/2 lg:basis-1/3 pl-2 md:pl-4'>
-            <div className='p-1'>
-              <Card>
-                <CardContent className='flex flex-col aspect-square items-center justify-evenly p-6 font-medium md:text-lg text-pretty'>
-                  <blockquote>
-                    <p>
-                      &laquo; On est venues par hasard avec ma copine et on
-                      s'est éclatées&nbsp;! &raquo;
-                    </p>
-                  </blockquote>
-                  <div className='text-slate-500 capitalize italic text-base'>
-                    Marion, Étudiante en prépa HEC
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-
-          <CarouselItem className='md:basis-1/2 lg:basis-1/3 pl-2 md:pl-4'>
-            <div className='p-1'>
-              <Card>
-                <CardContent className='flex flex-col aspect-square items-center justify-evenly p-6 font-medium md:text-lg text-pretty'>
-                  <blockquote>
-                    <p>
-                      &laquo; J'ai pu confier mon fils à Laurent le temps de
-                      faire mes courses et il ne voulait plus quitter le
-                      babyfoot. &raquo;
-                    </p>
-                  </blockquote>
-                  <div className='text-slate-500 capitalize italic text-base'>
-                    Henry, Franc-Maçon
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem> */}
         </CarouselContent>
       </Carousel>
     </section>
