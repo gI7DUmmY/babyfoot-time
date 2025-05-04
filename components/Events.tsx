178ae6184357056b20e2545f0192a205894e7655
Cedric Rollo <cedric.rollo@gmail.com>
2025-05-04 16:19:38 +0200

    💬 events

--- components/Events.tsx
+++ components/Events.tsx
@@ -25,7 +25,7 @@ import {
 import Link from 'next/link'
 import { FaCalendarCheck } from 'react-icons/fa6'
 import { GiClick } from 'react-icons/gi'
-import { PiNumberCircleTwoFill } from 'react-icons/pi'
+import { PiNumberCircleOneFill } from 'react-icons/pi'
 
 export function Events() {
   const [open, setOpen] = React.useState(false)
@@ -43,8 +43,8 @@ export function Events() {
           >
             <div className='relative'>
               <span className='absolute flex h-6 w-6 -top-3 -right-4'>
-                <PiNumberCircleTwoFill className='animate-ping absolute inline-flex h-full w-full rounded-full bg-orange1 opacity-75' />
-                <PiNumberCircleTwoFill className='relative inline-flex rounded-full h-6 w-6 group-hover:text-orange1' />
+                <PiNumberCircleOneFill className='animate-ping absolute inline-flex h-full w-full rounded-full bg-orange1 opacity-75' />
+                <PiNumberCircleOneFill className='relative inline-flex rounded-full h-6 w-6 group-hover:text-orange1' />
               </span>
               <div className='flex place-content-center items-baseline'>
                 <FaCalendarCheck className='mr-2 text-xl md:text-2xl' />
@@ -61,17 +61,6 @@ export function Events() {
             </DialogTitle>
             <div className='text-xl lg:text-2xl'>
               <ul className='flex flex-col items-start gap-4'>
-                <li>
-                  le 3/05 au Fût et à Mesure :
-                  <Link
-                    href='https://tally.so/r/mOEaEk'
-                    target='_blank'
-                    className='ml-2 p-1 font-playfair text-xl text-orange2 hover:text-background hover:bg-orange2 hover:rounded-2xl lg:text-3xl'
-                  >
-                    Je m'inscris !
-                    <GiClick className='ml-2 inline align-middle' />
-                  </Link>
-                </li>
                 <li>
                   le 24/05 au Fût et à Mesure :
                   <Link
@@ -103,8 +92,8 @@ export function Events() {
         >
           <div className='relative'>
             <span className='absolute flex h-6 w-6 -top-3 -right-4'>
-              <PiNumberCircleTwoFill className='animate-ping absolute inline-flex h-full w-full rounded-full bg-orange1 opacity-75' />
-              <PiNumberCircleTwoFill className='relative inline-flex rounded-full h-6 w-6 group-hover:text-orange2' />
+              <PiNumberCircleOneFill className='animate-ping absolute inline-flex h-full w-full rounded-full bg-orange1 opacity-75' />
+              <PiNumberCircleOneFill className='relative inline-flex rounded-full h-6 w-6 group-hover:text-orange2' />
             </span>
             <div className='flex place-content-center items-baseline'>
               <FaCalendarCheck className='mr-2 text-xl md:text-4xl' />
@@ -121,18 +110,6 @@ export function Events() {
           </DrawerTitle>
           <div className='text-xl my-4'>
             <ul className='flex flex-col gap-4 items-center text-center'>
-              <li>
-                le 3/05 au Fût et à Mesure
-                <br />
-                <Link
-                  href='https://tally.so/r/mOEaEk'
-                  target='_blank'
-                  className='ml-2 font-playfair text-xl text-orange2'
-                >
-                  Je m'inscris !
-                  <GiClick className='ml-2 inline align-middle' />
-                </Link>
-              </li>
               <li>
                 le 24/05 au Fût et à Mesure
                 <br />
