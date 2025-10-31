c5f189f0456d176c4e3a65e832bbe00997ecde74
Ced <cedric.rollo@gmail.com>
2025-10-31 12:01:42 +0100

    💬 tarif tournois

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,23 +4,23 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/ficheTarif'
-import { FaCheck } from 'react-icons/fa'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import { TiArrowBack } from 'react-icons/ti'
-import { PiListChecksFill } from 'react-icons/pi'
+} from "@/components/ui/ficheTarif";
+import { FaCheck } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { TiArrowBack } from "react-icons/ti";
+import { PiListChecksFill } from "react-icons/pi";
 
 const page = () => {
   return (
     <section>
-      <div className='pt-8 flex flex-col gap-2 px-2 md:items-center lg:flex-row lg:items-stretch lg:place-content-center'>
+      <div className="pt-8 flex flex-col gap-2 px-2 md:items-center lg:flex-row lg:items-stretch lg:place-content-center">
         {/* Coaching */}
         <Card>
           <CardHeader>
             <CardTitle>
-              <div className='text-orange1'>Coaching</div>
-              <div className='lining-nums'>100&euro;</div>
+              <div className="text-orange1">Coaching</div>
+              <div className="lining-nums">100&euro;</div>
             </CardTitle>
             <CardDescription>
               Cours à domicile : initiation ou perferctionnement
@@ -29,18 +29,18 @@ const page = () => {
           <CardContent>
             <ul>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />1 heure de
+                <FaCheck className="inline text-orange1 mr-2" />1 heure de
                 coaching
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />1 joueur pro
+                <FaCheck className="inline text-orange1 mr-2" />1 joueur pro
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />1 table de
+                <FaCheck className="inline text-orange1 mr-2" />1 table de
                 compétition Bonzini (B90 ITSF)
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Frais de déplacement offerts dans un rayon de 50Km de Toulouse
               </li>
             </ul>
@@ -51,8 +51,8 @@ const page = () => {
         <Card>
           <CardHeader>
             <CardTitle>
-              <div className='text-orange1'>Tournoi</div>
-              <div className='lining-nums'>750&euro;</div>
+              <div className="text-orange1">Tournoi</div>
+              {/*<div className='lining-nums'>750&euro;</div>*/}
             </CardTitle>
             <CardDescription>
               Tournoi clé en main pour environ 30 personnes
@@ -61,19 +61,19 @@ const page = () => {
           <CardContent>
             <ul>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />1 animateur
+                <FaCheck className="inline text-orange1 mr-2" />1 animateur
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />3 tables de
+                <FaCheck className="inline text-orange1 mr-2" />3 tables de
                 compétition Bonzini (B90 ITSF)
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Acceuil-briefing-phase qualificative et phase finale-remise des
                 prix
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Visite gratuite avant événement pour étude de faisabilité
               </li>
             </ul>
@@ -84,8 +84,8 @@ const page = () => {
         <Card>
           <CardHeader>
             <CardTitle>
-              <div className='text-orange1'>Entretien</div>
-              <div className='lining-nums'>100&euro;</div>
+              <div className="text-orange1">Entretien</div>
+              <div className="lining-nums">100&euro;</div>
             </CardTitle>
             <CardDescription>
               Révision et entretien de votre baby-foot
@@ -94,27 +94,27 @@ const page = () => {
           <CardContent>
             <ul>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Nettoyage du meuble
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Démontage et nettoyage en profondeur des barres et des joueurs
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Dégraissage du tapis
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Lubrification des barres
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Réalignement des joueurs
               </li>
               <li>
-                <FaCheck className='inline text-orange1 mr-2' />
+                <FaCheck className="inline text-orange1 mr-2" />
                 Changement de pièces à envisager sur devis
               </li>
             </ul>
@@ -122,31 +122,31 @@ const page = () => {
         </Card>
       </div>
 
-      <div className='flex place-content-center gap-6 mt-4'>
+      <div className="flex place-content-center gap-6 mt-4">
         <Button
-          variant='outline'
+          variant="outline"
           asChild
-          className='border-orange2 p-3 font-playfair text-2xl text-orange2 hover:text-background hover:bg-orange2 md:p-6 md:text-4xl'
+          className="border-orange2 p-3 font-playfair text-2xl text-orange2 hover:text-background hover:bg-orange2 md:p-6 md:text-4xl"
         >
-          <Link href='/#maintenance'>
-            <TiArrowBack className='mr-2 text-xl md:text-4xl' />
+          <Link href="/#maintenance">
+            <TiArrowBack className="mr-2 text-xl md:text-4xl" />
             Accueil
           </Link>
         </Button>
 
         <Button
-          variant='outline'
+          variant="outline"
           asChild
-          className='border-orange2 p-3 font-playfair text-2xl text-orange2 hover:text-background hover:bg-orange2 md:p-6 md:text-4xl'
+          className="border-orange2 p-3 font-playfair text-2xl text-orange2 hover:text-background hover:bg-orange2 md:p-6 md:text-4xl"
         >
-          <Link href='https://tally.so/r/w4pxrd' target='_blank'>
-            <PiListChecksFill className='mr-2 text-xl md:text-4xl' />
+          <Link href="https://tally.so/r/w4pxrd" target="_blank">
+            <PiListChecksFill className="mr-2 text-xl md:text-4xl" />
             Devis à la carte
           </Link>
         </Button>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
