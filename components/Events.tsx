a950de9bf08505562f7f7e8f8a48e2bb109e81fc
Ced <cedric.rollo@gmail.com>
2026-02-16 18:28:46 +0100

    💬 upd events

--- components/Events.tsx
+++ components/Events.tsx
@@ -26,7 +26,7 @@ import Link from 'next/link'
 import { FaCalendarCheck } from 'react-icons/fa6'
 import { GiClick } from 'react-icons/gi'
 import { PiQuestionFill } from 'react-icons/pi'
-import { PiNumberCircleTwoFill } from 'react-icons/pi'
+import { PiNumberCircleThreeFill } from 'react-icons/pi'
 
 export function Events() {
   interface Event {
@@ -49,6 +49,11 @@ export function Events() {
       date: new Date('2026-03-03T17:00'),
       lieu: 'Au Carré des Sports',
       link: 'https://tally.so/r/pbD0Z8'
+    },
+    {
+      date: new Date('2026-03-21T19:00'),
+      lieu: 'Au Fût et À Mesure',
+      link: 'https://tally.so/r/OD7jVY'
     }
   ]
 
@@ -62,8 +67,8 @@ export function Events() {
         </span>
       ) : (
         <span className='absolute flex h-6 w-6 -top-3 -right-4'>
-          <PiNumberCircleTwoFill className='animate-ping absolute inline-flex h-full w-full rounded-full bg-orange1 opacity-75' />
-          <PiNumberCircleTwoFill className='relative inline-flex rounded-full h-6 w-6 group-hover:text-orange1' />
+          <PiNumberCircleThreeFill className='animate-ping absolute inline-flex h-full w-full rounded-full bg-orange1 opacity-75' />
+          <PiNumberCircleThreeFill className='relative inline-flex rounded-full h-6 w-6 group-hover:text-orange1' />
         </span>
       )}
     </>
