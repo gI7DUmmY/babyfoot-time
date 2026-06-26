2188a78cfae512bfe9a5343fe751b9621272d7fd
Ced <cedric.rollo@gmail.com>
2026-06-26 15:16:51 +0200

    🎨 fetch des images

--- components/CarouselCollab.tsx
+++ components/CarouselCollab.tsx
@@ -12,30 +12,31 @@ const CarouselCollab = async ({ BUCKET, PREFIX }: params) => {
 
   return (
     <div className='snap-x snap-mandatory scroll-ps-6 flex gap-4 overflow-x-auto p-4'>
-      {files.map((f, index) => {
-        const filePath = PREFIX.endsWith('/')
-          ? `${PREFIX}${f.name}`
-          : `${PREFIX}/${f.name}`
-        const url = publicUrl(BUCKET, filePath)
-        return (
-          <figure
-            key={f.id}
-            className='snap-center flex shrink-0 w-[80vw] rounded-lg overflow-hidden bg-background md:w-[40vw] lg:w-[30vw]'
-          >
-            <div className='relative w-full h-[60vh]'>
-              <Image
-                src={url}
-                alt={f.name}
-                fill
-                className='object-cover'
-                priority={index < 3 ? true : false}
-                loading={index < 3 ? 'eager' : 'lazy'}
-                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-              />
-            </div>
-          </figure>
-        )
-      })}
+      {files.length > 0 &&
+        files.map((f, index) => {
+          const filePath = PREFIX.endsWith('/')
+            ? `${PREFIX}${f.name}`
+            : `${PREFIX}/${f.name}`
+          const url = publicUrl(BUCKET, filePath)
+          return (
+            <figure
+              key={f.id}
+              className='snap-center flex shrink-0 w-[80vw] rounded-lg overflow-hidden bg-background md:w-[40vw] lg:w-[30vw]'
+            >
+              <div className='relative w-full h-[60vh]'>
+                <Image
+                  src={url}
+                  alt={f.name}
+                  fill
+                  className='object-cover'
+                  priority={index < 3 ? true : false}
+                  loading={index < 3 ? 'eager' : 'lazy'}
+                  sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                />
+              </div>
+            </figure>
+          )
+        })}
     </div>
   )
 }
